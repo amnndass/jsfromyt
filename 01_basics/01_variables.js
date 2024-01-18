849463db29e5239b8bfa195e132cbd52950d80d3
amnndass <156493099+amnndass@users.noreply.github.com>
2024-01-18 13:45:48 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 2322
+
+//accountId = 5544  //not allowed
+
+console.log(accountId);
+
+accountEmail = "this@gmail.com";
+accountPassword = "5394573"
+console.table([accountId, accountEmail, accountPassword])
